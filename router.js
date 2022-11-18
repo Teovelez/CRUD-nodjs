422e3d73c96711bc05dc57a7c79726ad2608cebb
Teovelez <111604523+Teovelez@users.noreply.github.com>
2022-11-18 09:55:25 -0500

    Add files via upload

---
+++ router.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+
+const conexion = require("./database/db");
+
+router.get("/", (req, res)=>{
+    conexion.query("SELECT * FROM users", (error, results)=>{
+        if(error){
+            throw error;
+
+        }else{
+            res.render("index", {results:results});
+        }
+    })
+})
+
+module.exports = router;
+
+//Ruta para crea registros
+
+router.get("/create", (req, res)=>{
+    res.render("create")
+})
+//Ruta para guardar
+const crud = require("./controllers/crud");
+router.post("/save", crud.save);
+
+router.post("/update", crud.update);
+
+//Ruta para editar registros
+router.get("/edit/:id", (req, res)=>{
+    const id = req.params.id;
+    conexion.query("SELECT * FROM users WHERE id=?",[id], (error, results)=>{
+        if(error){
+            throw error;
+
+        }else{
+            res.render("edit", {user:results [0]});
+        }
+
+    })
+})
+//Ruta para eliminar
+router.get("/delete/:id", (req, res)=>{
+    const id = req.params.id;
+    conexion.query("DELETE FROM users WHERE id = ?", [id], (error, results)=>{
+        if(error){
+            throw error;
+
+        }else{
+            res.redirect("/");
+        }
+
+    })
+})
